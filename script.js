22b164dc261e66156369cf8edc97fe10f8759c2e
yincheng-dev <70377643+yincheng-dev@users.noreply.github.com>
2021-04-20 14:38:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
  <script>
-//Calculate Tip
+
 function calculateTip() {
   var billAmt = document.getElementById("billamt").value;
   var serviceQual = document.getElementById("serviceQual").value;
   var numOfPeople = document.getElementById("peopleamt").value;
 
-  //validate input
+ 
   if (billAmt === "" || serviceQual == 0) {
     alert("Please enter values");
     return;
   }
-  //Check to see if this input is empty or less than or equal to 1
+ 
   if (numOfPeople === "" || numOfPeople <= 1) {
     numOfPeople = 1;
     document.getElementById("each").style.display = "none";
@@ -20,7 +20,7 @@ function calculateTip() {
 
   //Calculate tip
   var total = (billAmt * serviceQual) / numOfPeople;
-  //round to two decimal places
+  //round to 
   total = Math.round(total * 100) / 100;
   //next line allows us to always have two digits after decimal point
   total = total.toFixed(2);
@@ -30,11 +30,9 @@ function calculateTip() {
 
 }
 
-//Hide the tip amount on load
 document.getElementById("totalTip").style.display = "none";
 document.getElementById("each").style.display = "none";
 
-//click to call function
 document.getElementById("calculate").onclick = function() {
   calculateTip();
 
